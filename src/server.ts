a7a09740cdb0be067270df6ebab6e1f88e136764
akormous <iamakshatchauhan@gmail.com>
2023-08-02 18:52:21 +0530

    Add Swagger doc at base path/doc

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express';
 import { app_v1 } from './v1';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../swagger_output.json';
 
 const app = express();
 
 app.use('/api/v1', app_v1);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(3000, () => {
   console.log("Server listening on port: 3000");
